b67a9e21aff4ee77309c6c1bf8024d12eb7de9f0
shaoyuanchao <497302807@qq.com>
2018-05-21 14:52:05 +0800

    Update rpt_overview.php

--- php/db/rpt_overview.php
+++ php/db/rpt_overview.php
@@ -1,4 +1,83 @@
 <?php
+
+//======================================
+// 函数: 获取$url_key相似的七日内日统计报表
+// 参数: $url_key           访问网址url
+// 返回: 七日内统计数据
+//======================================
+function get_rpt_overview_detail($url_key,$today)
+{
+  $endtoday = $today + 24*60*60;
+  $db = new DB_WWW();
+  $sql = "SELECT * FROM rpt_period_url_action WHERE action_url  like '%{$url_key}%' AND from_time_stamp >= '{$today}' AND to_time_stamp <='{$endtoday}'";
+
+  $db->query($sql);
+  $rows = $db->fetchAll();
+  return $rows;
+}
+
+// ======================================
+// 函数: 获取$url_key相似的当前周统计报表
+// 参数: $url_key           访问网址url
+// 返回: 一周统计数据
+//======================================
+function get_week_overview($url_key)
+{
+  $endLastweek=mktime(23,59,59,date('m'),date('d')-date('w')+7-7,date('Y'));
+  $week_time = $endLastweek - 7*24*60*60;
+  $db = new DB_WWW();
+  $sql = "SELECT * FROM rpt_period_url_action WHERE action_url  like '%{$url_key}%' AND from_time_stamp >= '{$week_time}' AND to_time_stamp <='{$endLastweek}'";
+  $db->query($sql);
+  $rows = $db->fetchAll();
+  return $rows;
+}
+
+//======================================
+// 函数: 获取总访问量
+// 参数: $url_key           访问网址url
+// 返回: 总访数组
+// //======================================
+function get_all_action($url_key)
+{
+  $db = new DB_WWW();
+
+  $sql = "SELECT * FROM cnt_url_action WHERE action_url like '%{$url_key}%'";
+  $db  -> query($sql);
+  $rows = $db->fetchAll();
+  return $rows;
+}
+
+
+//======================================
+// 函数: 查询$url_key相似的数据统计
+// 参数: $url_key           访问网址url_key
+//======================================
+
+function serch_rpt_detail($url_key, $today)
+{
+  $endtime = $today + 24 *60*60;
+  $db = new DB_WWW();
+
+  $sql = "SELECT * FROM cnt_url_action WHERE action_url like '%{$url_key}%' AND action_time >= '{$today}' AND action_time <= '{$endtime}'";
+  $db  -> query($sql);
+  $rows = $db->fetchAll();
+  return $rows;
+}
+
+//======================================
+// 函数: 创建网址访问统计记录
+// 参数: $data          信息数组
+//======================================
+function creat_rpt_detail($data)
+{
+  // 提交时间
+  $db = new DB_WWW();
+  $sql = $db->sqlInsert("rpt_period_url_action", $data);
+  $q_id = $db->query($sql);
+  if ($q_id == 0)
+    return 0;
+  return $db->insertID();
+}
 //======================================
 // 函数: 取得概要统计报告所有记录
 // 参数: 无
