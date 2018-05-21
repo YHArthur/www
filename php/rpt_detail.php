7f1fac0522cb37eea4385efec99b75f006642180
shaoyuanchao <497302807@qq.com>
2018-05-21 15:26:23 +0800

    Add files via upload

---
+++ php/rpt_detail.php
@@ -0,0 +1,73 @@
+<?php
+require_once 'inc/common.php';
+require_once 'db/rpt_overview.php';
+require_once 'db/rpt_period_url_action.php';
+require_once 'db/cnt_url_action.php';
+
+
+
+header("cache-control:no-cache,must-revalidate");
+header("Content-Type:application/json;charset=utf-8");
+
+php_begin();
+
+$args = array('url_key');
+chk_empty_args('GET', $args);
+$count_num = array();
+$data = array();
+$pout = array();
+// 提交参数整理
+$url_key = get_arg_str('GET', 'url_key');
+//取得当天零点的时间戳
+$today = strtotime(date('Y-m-d', time()));
+$i = 0;
+//获取周浏览数数组
+$week_rows = get_week_overview($url_key);
+foreach ($week_rows as $week_row) {
+  $i += $week_row['action_count'];
+}
+
+//获取总浏览数组
+$all_action = get_all_action($url_key);
+
+$begin_time = $today - 7*24*60*60;
+while($today){
+  
+  $rows = get_rpt_overview_detail($url_key, $today);
+  if(count($rows)){
+    foreach($rows as $row){
+      $count_num['rpt_title'] =substr($row['rpt_title'],5,10);
+      $count_num['action_count'] = $row['action_count'];
+      $pout[] = $count_num;
+    }
+  }
+  else{
+    $count = serch_rpt_detail($url_key, $today);
+    $unio = count($count);
+    $data['action_url'] = $url_key;
+    $data['rpt_title'] =date("Y-m-d",$today);
+    $data['from_time_stamp'] = $today;
+    $data['to_time_stamp']  = $today + 24*60*60;
+    $data['action_count'] = $unio;
+    $creat = creat_rpt_detail($data);
+    array_splice($data, 0, count($data));
+    continue;
+  }
+  $today =$today - 24*60*60;
+  if($today == $begin_time){
+    $today = 0;
+  }
+  $pout["action_url"] = $row['action_url'];
+}
+
+//返回数据做成
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = '';
+$rtn_ary['week_action'] =$i;
+$rtn_ary['all_action'] =count($all_action);
+$rtn_ary['rows'] = $pout;
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
+
+?>
