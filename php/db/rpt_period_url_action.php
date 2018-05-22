b816133f9d3e12143b37e7e13006f7a6d4ce9fcf
shaoyuanchao <497302807@qq.com>
2018-05-22 13:19:46 +0800

    Update rpt_period_url_action.php

--- php/db/rpt_period_url_action.php
+++ php/db/rpt_period_url_action.php
@@ -2,27 +2,34 @@
 
 //======================================
 // 函数: 获取$url_key相似的每日日统计报表
-// 参数: $url_key           访问网址url
-// 返回: 每日统计数据
+// 参数: $url_key            url关键字
+//       $today             起始时间
+//       $endtoday          截至时间
+// 返回: 记录数组
+//       rpt_title          标题
+//       action_count        数量
 //======================================
-function get_day_overview($url_key,$today,$endtoday)
+function get_rpt_overview_detail($url_key,$today,$endtoday)
 {
   $db = new DB_WWW();
-  $sql = "SELECT * FROM rpt_period_url_action WHERE action_url  like '%{$url_key}%' AND from_time_stamp >= '{$today}' AND to_time_stamp <='{$endtoday}' AND rpt_type = 'day'";
+  $sql = "SELECT * FROM rpt_period_url_action WHERE action_url = '{$url_key}' AND from_time_stamp >= '{$today}' AND to_time_stamp <='{$endtoday}' AND rpt_type = 'day'";
   $db->query($sql);
-  $rows = $db->fetchAll();
+  $rows = $db->fetchRow();
   return $rows;
 }
 
 // ======================================
 // 函数: 获取$url_key相似的当前周统计报表
-// 参数: $url_key           访问网址url
-// 返回: 一周统计数据
+// 参数: $url_key           url关键字
+//       $week_time         数据获取起始时间
+//       $endLastweek       数据获取截至时间
+// 返回: url_count           访问总数 
+//      
 //======================================
 function get_week_overview($url_key,$week_time,$endLastweek)
 {
   $db = new DB_WWW();
-  $sql = "SELECT action_count FROM rpt_period_url_action WHERE action_url  like '%{$url_key}%' AND from_time_stamp >= '{$week_time}' AND to_time_stamp <='{$endLastweek}' AND rpt_type='week' ";
+  $sql = "SELECT action_count FROM rpt_period_url_action WHERE action_url  = '{$url_key}' AND from_time_stamp >= '{$week_time}' AND to_time_stamp <='{$endLastweek}' AND rpt_type='week' ";
   $url_count = $db->getField($sql, 'action_count');
   return $url_count;
 }
@@ -30,6 +37,8 @@ function get_week_overview($url_key,$week_time,$endLastweek)
 //======================================
 // 函数: 创建网址访问统计记录
 // 参数: $data          信息数组
+// 返回: 0              创建失败
+//      创建id          成功
 //======================================
 function creat_rpt_detail($data)
 {
