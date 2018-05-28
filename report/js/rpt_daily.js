6d981ddabc31f616d685672dee9f7ef1afbf94b2
YHArthur <34911254+YHArthur@users.noreply.github.com>
2018-05-28 20:00:11 +0800

    0528

--- report/js/rpt_daily.js
+++ report/js/rpt_daily.js
@@ -1,6 +1,5 @@
 $(function () {
     var url_key = GetQueryString('url_key');
-    $("#url_key").html(url_key);
     // 更新并展示网站日报表
     rpt_daily_refresh(url_key);
 })
@@ -15,6 +14,7 @@ function rpt_daily_refresh(url_key) {
 function rpt_daily(url_key) {
     var api_url = 'rpt_daily.php?url_key=' + url_key;
     CallApi(api_url, {}, function (response) {
+        $("#url_key").html(url_key);
         $("#rpt_title").html(response.rpt_title);
         $("#daily_sum").html(response.daily_sum);
         $("#rpt_unit").html(response.rpt_unit);
